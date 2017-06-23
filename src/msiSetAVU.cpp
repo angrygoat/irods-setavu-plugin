4a9201afadc0dc9eff75f01af13fc9d5a10be8c6
Jason Coposky <jasonc@renci.org>
2017-06-23 15:39:39 -0400

    update microservice and build environment for 4-2-stable

--- src/msiSetAVU.cpp
+++ src/msiSetAVU.cpp
@@ -1,34 +1,79 @@
 #define RODS_SERVER 1
 
 #include <cstddef>
-#include "reFuncDefs.hpp"
 
-#include "microservice.hpp"
-#include "modAVUMetadata.hpp"
+#include "irods_error.hpp"
+#include "irods_ms_plugin.hpp"
+#include "rsModAVUMetadata.hpp"
 
+int msiSetAVU(
+    msParam_t* _item_type,
+    msParam_t* _item_name,
+    msParam_t* _attr_name,
+    msParam_t* _attr_val,
+    msParam_t* _attr_unit,
+    ruleExecInfo_t* _rei ) {
 
-MICROSERVICE_BEGIN(
-    msiSetAVU,
-    STR, itemType, INPUT,
-    STR, itemName, INPUT,
-    STR, attrName, INPUT,
-    STR, attrVal,  INPUT,
-    STR, attrUnit, INPUT )
+    char *it_str = parseMspForStr( _item_type );
+    if( !it_str ) {
+        return SYS_INVALID_INPUT_PARAM;
+    }
 
-char op[]  = "set";
+    char *in_str = parseMspForStr( _item_name );
+    if( !in_str ) {
+        return SYS_INVALID_INPUT_PARAM;
+    }
 
-modAVUMetadataInp_t avuOp;
-memset(&avuOp, 0, sizeof(avuOp));
-avuOp.arg0 = op;
-avuOp.arg1 = itemType;
-avuOp.arg2 = itemName;
-avuOp.arg3 = attrName;
-avuOp.arg4 = attrVal;
-avuOp.arg5 = attrUnit;
+    char *an_str = parseMspForStr( _attr_name );
+    if( !an_str ) {
+        return SYS_INVALID_INPUT_PARAM;
+    }
 
-rei->status = rsModAVUMetadata(rei->rsComm, &avuOp);
+    char *av_str = parseMspForStr( _attr_val );
+    if( !av_str ) {
+        return SYS_INVALID_INPUT_PARAM;
+    }
 
-RETURN(rei->status);
+    char *au_str = parseMspForStr( _attr_unit );
+    if( !au_str ) {
+        return SYS_INVALID_INPUT_PARAM;
+    }
 
-MICROSERVICE_END
+    char op[]  = "set";
+
+    modAVUMetadataInp_t avuOp;
+    memset(&avuOp, 0, sizeof(avuOp));
+    avuOp.arg0 = op;
+    avuOp.arg1 = it_str;
+    avuOp.arg2 = in_str;
+    avuOp.arg3 = an_str;
+    avuOp.arg4 = av_str;
+    avuOp.arg5 = au_str;
+
+    _rei->status = rsModAVUMetadata(_rei->rsComm, &avuOp);
+
+    return _rei->status;
+
+}
+
+extern "C"
+irods::ms_table_entry* plugin_factory() {
+    irods::ms_table_entry* msvc = new irods::ms_table_entry(5);
+
+    msvc->add_operation<
+        msParam_t*,
+        msParam_t*,
+        msParam_t*,
+        msParam_t*,
+        msParam_t*,
+        ruleExecInfo_t*>("msiSetAVU",
+                         std::function<int(
+                             msParam_t*,
+                             msParam_t*,
+                             msParam_t*,
+                             msParam_t*,
+                             msParam_t*,
+                             ruleExecInfo_t*)>(msiSetAVU));
+    return msvc;
+}
 
